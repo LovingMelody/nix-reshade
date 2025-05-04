562e4b017bf8090b624768ee88757b446193f417
Melody Renata <me@lovingmelody.io>
2025-05-04 01:32:57 -0400

    d3dcompiler_47-dll: obtain from wine64

--- packages/d3dcompiler_47.dll/default.nix
+++ packages/d3dcompiler_47.dll/default.nix
@@ -1,15 +1,13 @@
 {
-  fetchurl,
+  wine64,
   stdenvNoCC,
   ...
 }:
 stdenvNoCC.mkDerivation {
   name = "d3dcompiler_47-dll";
-  src = fetchurl {
-    url = "https://lutris.net/files/tools/dll/d3dcompiler_47.dll";
-    hash = "sha256-6ZSEfgGm8eTL3FqGRhasJi9n7k8U2xlJhGYajZJ6t/Q=";
-  };
-  unpackPhase = ''
-    install -Dm644 -v $src $out/lib/d3dcompiler_47.dll
+  src = wine64;
+  phases = ["unpackPhase" "installPhase"];
+  installPhase = ''
+    install -Dm644 -v "$src/lib/wine/x86_64-windows/d3dcompiler_47.dll" "$out/lib/d3dcompiler_47.dll"
   '';
 }
